6ea3868e9e0fb64ca0e62110b9d0aa91269d6e07
LizzieDworsky <amydworsky15@gmail.com>
2022-05-24 14:34:10 -0600

    updated and tested product_detail function in views file + created pathway for function in urls

--- product/views.py
+++ product/views.py
@@ -18,7 +18,7 @@ def products_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
