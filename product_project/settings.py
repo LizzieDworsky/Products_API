3938ec2060826a177a6d3c74818035201fa55d43
LizzieDworsky <amydworsky15@gmail.com>
2022-05-24 09:04:32 -0600

    changed database settings + created product app

--- product_project/settings.py
+++ product_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g(6r_l$axu1o0nv1k6ix*)l%vhv%_&cbjb@wxfmskfzq-f5uwf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
