73a1fe1cd9ce609658dbf71527c92811f5211796
LizzieDworsky <amydworsky15@gmail.com>
2022-05-24 13:00:57 -0600

    added POST to function, untested, + register product model in admin

--- product/views.py
+++ product/views.py
@@ -1,6 +1,18 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    pass
+    if request.method == 'GET':
+        products = Product.object.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
