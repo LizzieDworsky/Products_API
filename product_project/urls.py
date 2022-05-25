f10e011d56159bc895a2e5e720791f72909396ea
LizzieDworsky <amydworsky15@gmail.com>
2022-05-25 08:34:10 -0600

    edited the urls project path from product to products + added status responses to the products list and detail

--- product_project/urls.py
+++ product_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/product/', include('product.urls')),
+    path('api/products/', include('product.urls')),
 ]
